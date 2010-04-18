e2fa8973d0e13d8f97b615bb28b8463dd90bcd5f
Kyle Neath <kneath@gmail.com>
2010-04-17 18:01:10 -0700

    Use new HTML to center

--- src/facebox.js
+++ src/facebox.js
@@ -124,7 +124,7 @@
       $('#facebox .content').append(data)
       $('#facebox .loading').remove()
       $('#facebox .body').children().fadeIn('normal')
-      $('#facebox').css('left', $(window).width() / 2 - ($('#facebox table').width() / 2))
+      $('#facebox').css('left', $(window).width() / 2 - ($('#facebox .popup').width() / 2))
       $(document).trigger('reveal.facebox').trigger('afterReveal.facebox')
     },
 
